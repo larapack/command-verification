2f0f72094fe189ba1f72ad8636b71366617b501a
Mark Topper <mail@webman.io>
2015-11-30 07:57:48 -0500

    Applied fixes from StyleCI

--- src/Larapack/CommandVerification/Verifiable.php
+++ src/Larapack/CommandVerification/Verifiable.php
@@ -4,20 +4,24 @@ namespace Larapack\CommandVerification;
 
 trait Verifiable
 {
-	protected function verify($message = null, $callback = null)
-	{
-	   if ($this->verify || $message)
-	   {
-		   if ($message) $this->warn($message);
-		   else $this->warn($this->verify);
-		   
-		   if (!$this->confirm('Do you wish to continue? [y|N]'))
-		   {
-			   return $this->comment('Command stopped...');
-		   }
-	   }
-	   
-	   if ($callback) return $callback();
-	   else return $this->verified();
-	}
+    protected function verify($message = null, $callback = null)
+    {
+        if ($this->verify || $message) {
+            if ($message) {
+                $this->warn($message);
+            } else {
+                $this->warn($this->verify);
+            }
+
+            if (!$this->confirm('Do you wish to continue? [y|N]')) {
+                return $this->comment('Command stopped...');
+            }
+        }
+
+        if ($callback) {
+            return $callback();
+        } else {
+            return $this->verified();
+        }
+    }
 }
